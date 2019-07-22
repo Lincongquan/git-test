c6d55e92f89f53260f88442248b36b53a259970c
Lincongquan <congquan@itcast.cn>
2019-07-22 21:08:51 +0800

    把桥接模式的案例添加到git

--- git_sayhello/src/cn/itcast/Girl_choose.java
+++ git_sayhello/src/cn/itcast/Girl_choose.java
@@ -2,12 +2,12 @@ package cn.itcast;
 
 public class Girl_choose  {
     public static void main(String[] args) {
-
+        //黑色包包
         Black_Color black_color = new Black_Color();
         Circleshape circleshape = new Circleshape();
         circleshape.setColor(black_color);
         circleshape.draw();
-
+        //蓝色包包
         Blue_Color blue_color = new Blue_Color();
         Rectangleshape rectangleshape = new Rectangleshape();
         rectangleshape.setColor(blue_color);
